c8165abd91ba801e4ca7a15259c4084a43afbbe2
bewar.gohdar <bewar.00429239@gmail.com>
2024-12-03 21:00:24 +0300

    feat: display real-time chat messages with Firestore StreamBuilder

--- lib/core/helper/widget/chat_messages.dart
+++ lib/core/helper/widget/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,16 +6,36 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(
-          title: Text('Hello'),
-          subtitle: Text('This is a message'),
-          trailing: const CircleAvatar(
-            child: Text('A'),
-          ),
-        ),
-      ],
+    return StreamBuilder(
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages yet'),
+          );
+        }
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('An error occurred'),
+          );
+        }
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final chatData = snapshot.data!.docs[index];
+            return Text(chatData['text']);
+          },
+          itemCount: snapshot.data!.docs.length,
+        );
+      },
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
     );
   }
 }
